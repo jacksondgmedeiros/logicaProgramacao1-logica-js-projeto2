65e7bef9f507caf975d8e5d01f3a0ef7ba7cc7c2
Jackson Medeiros <jackson.dgmedeiros@gmail.com>
2024-04-12 20:52:58 -0300

    etapa 1 - interagindo com o HTML

---
+++ app.js
@@ -0,0 +1,14 @@
+
+// !-- inicio 
+//  define variável para pegar a tag e inserir um texto
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Jogo do número secreto';
+
+let paragrafo = document.querySelector('p');
+paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+// --! fim 
+
+// cria uma função que será chamada no html, dentro da tag button com a class container__botao
+function verificarChute(){
+
+}
